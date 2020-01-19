5ff7f52f585e08ca6155b37f7e36d41c27cce88f
efuious <efuious@gmail.com>
2020-01-19 15:12:31 +0800

    添加、修改文件

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import versions as v
+import versions as version
 
 version = '0.1.1'
 
 print ('Github version control test')
-print ('this version is: ',v.update1)
+print ('this version is: ',version.update1)
